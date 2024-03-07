03e0401b831cd076d4c877b985776289ecde36c4
Vitor Ribeiro <Vitorribeiro20022@gmail.com>
2024-03-07 10:06:28 +0000

    update

---
+++ Casa/printfStack.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    char pass[10] = "Vitor";
+    char string[100];
+
+    printf("Indique uma string");
+    scanf("%s",string);
+
+
+    printf(string); 
+
+    return 0;
+}
