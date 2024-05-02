dbe1aeaf15a7703efb0e775664db6ca70f342e7c
Vitor Ribeiro <Vitorribeiro20022@gmail.com>
2024-05-02 16:04:08 +0100

    Binary Tree

--- BinTree/BinTree.c
+++ BinTree/BinTree.c
@@ -1,15 +1,24 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STAGES 15
+
+typedef struct Jogador {
+  char nome[64];
+  int sets;
+
+} JOGADOR;
+
 typedef struct Tree {
 
-  int data;
+  JOGADOR *data;
   struct Tree *right;
   struct Tree *left;
 
 } TREENODE;
 
-TREENODE *CreateNode(int _data) { // Utilizado para iniciar uma Folha
+TREENODE *CreateNode(JOGADOR *_data) { // Utilizado para iniciar uma Folha
 
   TREENODE *node = (TREENODE *)malloc(sizeof(TREENODE));
   if (!node) {
@@ -23,7 +32,7 @@ TREENODE *CreateNode(int _data) { // Utilizado para iniciar uma Folha
   return node; // sem erros
 }
 
-TREENODE *CreateNodeInfo(int _data, TREENODE *_left,
+TREENODE *CreateNodeInfo(JOGADOR *_data, TREENODE *_left,
                          TREENODE *_right) { // Usado para iniciar um nó
 
   TREENODE *node = NULL;
@@ -36,32 +45,102 @@ TREENODE *CreateNodeInfo(int _data, TREENODE *_left,
   return node;
 }
 
-TREENODE *CreateBinTree(int *v, int i, int size) {
+TREENODE *CreateBinTree(JOGADOR *v, int i, int size) {
 
   if (i >= size) {
 
     return NULL;
   }
-  return CreateNodeInfo(*(v + i), CreateBinTree(v, 2 * i + 1, size),
+  return CreateNodeInfo((v + i), CreateBinTree(v, 2 * i + 1, size),
                         CreateBinTree(v, 2 * i + 2, size));
 }
-
-void PrintTreePreOrder(TREENODE *node) {
-
-  if (node == NULL) {
-    return;
+bool ReadFromFile(char *filename, JOGADOR **vetor) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) {
+    perror("Erro ao abrir ficheiro\n");
+    return false;
   }
-  printf("%i - ", node->data);
 
-  PrintTree(node->left);
-  PrintTree(node->right);
+  int i = 0;
+  char line[64];
+  while (fgets(line, 64, fp)) {
+    JOGADOR *jogador = (JOGADOR *)malloc(sizeof(JOGADOR));
+    if (!jogador) {
+      perror("Erro ao alocar memoria para jogador\n");
+      fclose(fp);
+      free(*vetor); // Free previously allocated memory
+      return false;
+    }
+    sscanf(line, "%[^;];%i", jogador->nome, &(jogador->sets));
+    (*vetor)[i] = *jogador; // Assign jogador to vetor[i]
+    i++;
+  }
+
+  fclose(fp);
+  return true;
+}
+void PrintArrayJogador(JOGADOR *v, int size) {
+
+  for (int i = 0; i < size; i++) {
+    printf("Jogador %i - \t nome: %s \t sets: %i \n", i, (v + i)->nome,
+           (v + i)->sets);
+  }
+}
+
+void PrintTreeJogadorPreOrder(TREENODE *node, int space) {
+
+  if (node == NULL)
+    return;
+  space += 15;
+
+  // Process right child first
+  PrintTreeJogadorPreOrder(node->right, space);
+
+  // Print current node after space
+  // count
+  printf("\n");
+  for (int i = 5; i < space; i++)
+    printf(" ");
+  printf("%s / %i\n", node->data->nome, node->data->sets);
+
+  // Process left child
+  PrintTreeJogadorPreOrder(node->left, space);
+}
+
+bool IsLeaf(TREENODE *Node) {
+  return (Node->left == NULL && Node->right == NULL);
+}
+
+int CountLeaf(TREENODE *btree) {
+  if (btree == NULL)
+    return 0;
+
+  return (IsLeaf(btree)) ? 1 : CountLeaf(btree->left) + CountLeaf(btree->right);
+}
+
+int maxNumber(int a, int b) { return (a > b) ? a : b; }
+
+int BtreeDepth(TREENODE *btree) {
+
+  if (btree == NULL)
+    return 0;
+  if (IsLeaf(btree))
+    return 1;
+
+  return 1 + maxNumber(BtreeDepth(btree->left), BtreeDepth(btree->right));
 }
 
 int main() {
-  int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
 
-  TREENODE *tree = CreateBinTree(array, 0, 10);
-  PrintTreePreOrder(tree);
+  JOGADOR *array = (JOGADOR *)malloc(STAGES * sizeof(JOGADOR));
+  ReadFromFile("Jogadores.txt", &array);
+  TREENODE *tree = CreateBinTree(array, 0, STAGES);
 
+  PrintTreeJogadorPreOrder(tree, BtreeDepth(tree));
+  // PrintArrayJogador(array, STAGES);
+
+  printf("\n");
+  printf("Tem %i Leafs\n", CountLeaf(tree));
+  printf("Tem %i Niveis\n", BtreeDepth(tree));
   return 0;
 }
